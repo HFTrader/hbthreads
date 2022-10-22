3e99af8a3e56b913f7692a15fd703f026451175f
Henrique Bucher <hbucher@gmail.com>
2022-10-21 20:15:07 -0500

    Add more documentation

--- BufferPrinter.h
+++ BufferPrinter.h
@@ -8,37 +8,93 @@
 
 template <size_t BUFSIZE>
 struct BufferPrinter {
+    //! Holds its own char buffer. The intent is to create this object in the stack
+    char _org[BUFSIZE];
+
+    //! Initializes to a single post
     BufferPrinter() : _ptr(_org) {
     }
-    char _org[BUFSIZE];
+
+    //! Current write pointer
     char* _ptr;
+
+    //! Number of already written bytes
     size_t size() const {
         return _ptr - _org;
     }
+
+    //! Helper - writes buffer to the given file descriptor
     int write(int fd) {
         return ::write(fd, _org, size());
     }
+
+    //! Helper - writes buffer to stdout
     int print() {
         return write(fileno(stdout));
     }
+
+    //! Helper - writes buffer to stderr
     int printerr() {
         return write(fileno(stderr));
     }
+
+    //! Saves the given string into the buffer. This will match pointers only
     friend BufferPrinter& operator<<(BufferPrinter& out, const char* str) {
         int nb = ::strlen(str);
         memcpy(out._ptr, str, nb);
         out._ptr += nb;
         return out;
     }
+
+    //! Saves the given string into the buffer. This will match literal char arrays.
     template <size_t N>
     friend BufferPrinter& operator<<(BufferPrinter& out, const char (&str)[N]) {
         memcpy(out._ptr, str, N - 1);
         out._ptr += N - 1;
         return out;
     }
+
+    // Stream/print 16 bit integers
+    friend BufferPrinter& operator<<(BufferPrinter& out, uint16_t value) {
+        out << "0x";
+        out.printhex(value);
+        return out;
+    }
+
+    // Stream/print 32 bit integers - but will print as 16 if small enough
+    friend BufferPrinter& operator<<(BufferPrinter& out, uint32_t value) {
+        if (uint16_t(value) == value) return out << uint16_t(value);
+        out << "0x";
+        out.printhex(value);
+        return out;
+    }
+
+    // Stream/print 64 bit integers - but will print as 32 if small enough
+    friend BufferPrinter& operator<<(BufferPrinter& out, uint64_t value) {
+        if (uint32_t(value) == value) return out << uint32_t(value);
+        out << "0x";
+        out.printhex(value);
+        return out;
+    }
+
+    // Stream/prints a pointer
+    friend BufferPrinter& operator<<(BufferPrinter& out, void* ptr) {
+        return out << reinterpret_cast<uint64_t>(ptr);
+    }
+
+    // Stream/prints an object pointer
+    template <typename T>
+    friend BufferPrinter& operator<<(BufferPrinter& out, T const* const ptr) {
+        return out << reinterpret_cast<uint64_t>(ptr);
+    }
+
+private:
+    //! Helper - returns the ascii digit given the value
     static int digit(int ch) {
         return ch < 10 ? '0' + ch : 'a' + (ch - 10);
     }
+
+    //! Very trivial asciifier of an integer value
     template <typename T>
     void printhex(const T& x) {
         constexpr size_t N = sizeof(T) * 2;  // 2 chars per byte
@@ -49,29 +105,5 @@ struct BufferPrinter {
             value /= 16;
         }
         _ptr += N;
-    }
-    friend BufferPrinter& operator<<(BufferPrinter& out, uint16_t value) {
-        out << "0x";
-        out.printhex(value);
-        return out;
-    }
-    friend BufferPrinter& operator<<(BufferPrinter& out, uint32_t value) {
-        if (uint16_t(value) == value) return out << uint16_t(value);
-        out << "0x";
-        out.printhex(value);
-        return out;
-    }
-    friend BufferPrinter& operator<<(BufferPrinter& out, uint64_t value) {
-        if (uint32_t(value) == value) return out << uint32_t(value);
-        out << "0x";
-        out.printhex(value);
-        return out;
-    }
-    friend BufferPrinter& operator<<(BufferPrinter& out, void* ptr) {
-        return out << reinterpret_cast<uint64_t>(ptr);
-    }
-    template <typename T>
-    friend BufferPrinter& operator<<(BufferPrinter& out, T const* const ptr) {
-        return out << reinterpret_cast<uint64_t>(ptr);
     }
 };
--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # hbthreads
 A coroutine-based reactor library with a few unique features:
 - simple to read for learning purposes
-- built-in intrusive pointer with integrated pool allocator
-- uncomplicated coroutine semantics based on very basic stack primitives from boost
-- event reactor with poll() and epoll() support 
-- reactor integrated with coroutines
+- minimal, voluntarily incomplete so it can be easily extended
+- built-in intrusive pointer with integrated pool allocator (no new/malloc calls)
+- uncomplicated coroutine semantics based on very basic (and fast) stack primitives 
+- event reactor with poll() and epoll() support (anyone still using select?)
+- coroutines embedded as reactor dispatch mechanism
 
 # Motivation
-The primary motivation is to provide some material for my substack blog articles
+The primary motivation is to show that network/socket/coroutine programming does not need
+to be necessarily complicated. I hope also to provide some learning material for my charity blog at 
+https://lucisqr.substack.com/
 
 # TODO
-- unit tests
-- performance tests
-- write an article on substack
+- Conan recipe
+- Unit Tests
+- Performance Tests
+-- Context switch 
+-- Latency tests
